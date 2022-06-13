bf05eceeeeac54466b828cdf6e48075b376e7cab
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-13 08:50:01 +0000

    [Finish]3-8 [Start]3-9

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -4,13 +4,13 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-  # 以下を追加
   def create
-    list = List.new(list_params)
-    list.save
-    # redirect_to '/top'を削除して、以下コードに変更
-    # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
 
