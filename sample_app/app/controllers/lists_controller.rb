7293fba0672008eadcecc3b8e65634522c455a47
EC2 Default User <ec2-user@ip-172-31-16-77.ec2.internal>
2022-06-13 08:23:10 +0000

    [Finish]3-7 [Start]3-8

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -32,6 +32,12 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
   private
   # ストロングパラメータ
   def list_params
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  # .../lists/1や.../lists/3に該当する
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists
+
 end
